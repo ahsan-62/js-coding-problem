f21ad1aadff4680cf1dff457732d790a842fbffb
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-04-05 00:21:30 +0600

    cheapest phone along the array

--- cheapest.js
+++ cheapest.js
@@ -1,18 +1,21 @@
 const phones=[
     { name:'iphone', price: 80000},
     { name:'samsung', price: 70000},
-    { name:'nokia', price: 30000},
+    { name:'nokia', price: 50000},
     { name:'htc', price: 40000},
 ]
 
 function cheapestPhone(phones){
     let cheapest=phones[0];
     for(let i=0;i<phones.length;i++){
-       if(phones[i].price<cheapest.price){
-           cheapest=phones[i];
+       const phone=phones[i];
+       if(phone.price<cheapest.price){
+           cheapest=phone;
        }
     }
-        
-}
+       return cheapest;
+} 
 
-cheapestPhone(phones)
+let mobile=cheapestPhone(phones);
+
+console.log(mobile);
