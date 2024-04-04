c19ac3378a0e12e381c7de91d3f1e23e9df2715e
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-04-04 17:24:23 +0600

    slice and spilce

---
+++ slice_splice.js
@@ -0,0 +1,17 @@
+//Slice in js Array
+
+let ahsan=[22,23,24,25,26,27,28,29,30];
+
+let myPart=ahsan.slice(2,6);
+
+console.log(myPart);
+
+//splice
+
+
+let jun =[1,2,3,4,5,6];
+
+let partial=jun.splice(1,3,8,9,10);
+console.log(partial);
+console.log(jun);
+
